5988a48c2de5b9f804c5c25b1c01b32effc85708
Isshiki_Hugh <2511524185@qq.com>
2022-04-29 09:45:38 +0800

    waiting for merge

--- main.go
+++ main.go
@@ -3,33 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"goodsman2.0/config"
 	"goodsman2.0/db"
 	"goodsman2.0/handlers"
 	"goodsman2.0/utils/feishu"
-
-	"github.com/sirupsen/logrus"
 )
 
-type LogFormatter struct{}
-
-func (slf *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	timestamp := time.Now().Local().Format("06/01/02 15:04:05")
-	msg := fmt.Sprintf("[%s] %s (%s:%d): %s\n", entry.Level,
-		timestamp,
-		filepath.Base(entry.Caller.File),
-		entry.Caller.Line,
-		entry.Message)
-	return []byte(msg), nil
-}
-
 func main() {
-
 	logrus.SetReportCaller(true)
-	logrus.SetFormatter(&LogFormatter{})
 	config.InitConfig()
 	db.Init()
 	feishu.Init()
